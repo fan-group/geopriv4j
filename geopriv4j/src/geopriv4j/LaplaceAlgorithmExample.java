d6a5788489ceba74683317c59baa51dc86dbe9cd
Sriram Gunja <sgunja@uncc.edu>
2020-06-12 15:01:57 -0400

    -updated Example classes

--- geopriv4j/src/geopriv4j/LaplaceAlgorithmExample.java
+++ geopriv4j/src/geopriv4j/LaplaceAlgorithmExample.java
@@ -4,24 +4,15 @@
 
 package geopriv4j;
 
-<<<<<<< HEAD
 import java.io.IOException;
-=======
-import java.io.IOException; 
->>>>>>> 19cc36cf79cb74fecace42cec1bb07417c4c0867
 import java.util.ArrayList;
 import geopriv4j.utils.DataHandler;
 import geopriv4j.utils.LatLng;
-
 
 public class LaplaceAlgorithmExample {
 
 	public static void main(String[] args) throws ClassNotFoundException, IOException {
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 19cc36cf79cb74fecace42cec1bb07417c4c0867
 		// ∊ value for ∊-differential privacy
 		double epsilon = 0.001;
 
@@ -35,11 +26,7 @@ public class LaplaceAlgorithmExample {
 		long startTime = System.currentTimeMillis();
 
 		for (int i = 0; i < locations.size(); i++) {
-<<<<<<< HEAD
 
-=======
-			
->>>>>>> 19cc36cf79cb74fecace42cec1bb07417c4c0867
 			LatLng generated_location = algorithm.generate(locations.get(i));
 
 			// System.out.println("Genereated location: "+ generated_location);
