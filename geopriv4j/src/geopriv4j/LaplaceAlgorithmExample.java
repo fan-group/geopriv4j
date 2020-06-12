0a2a9b0f6503fbeac33aff78af33a3bf5c0e4c53
fan-group <39656159+fan-group@users.noreply.github.com>
2020-06-12 11:08:05 -0400

    Update LaplaceAlgorithmExample.java

--- geopriv4j/src/geopriv4j/LaplaceAlgorithmExample.java
+++ geopriv4j/src/geopriv4j/LaplaceAlgorithmExample.java
@@ -22,15 +22,13 @@ public class LaplaceAlgorithmExample {
 
 	public static void main(String[] args) throws ClassNotFoundException, IOException {
 
-		// //this is the current user location
-		// LatLng current_loc = new LatLng(35.3123,-80.7432);
 
 		// ∊ value for ∊-differential privacy
 		double epsilon = 0.001;
 
 		LaplaceAlgorithm algorithm = new LaplaceAlgorithm(epsilon);
 
-		// change this variable to pick 1000, 5000, 10000 dummy points
+		// change this variable to pick 1000, 5000, 10000 synthetic points
 		int data = 5000;
 
 		ArrayList<LatLng> locations = DataHandler.readData("data/" + data + ".txt");
@@ -38,7 +36,7 @@ public class LaplaceAlgorithmExample {
 		long startTime = System.currentTimeMillis();
 
 		for (int i = 0; i < locations.size(); i++) {
-			// call all the locations as generated_locations
+			
 			LatLng generated_location = algorithm.generate(locations.get(i));
 
 			// System.out.println("Genereated location: "+ generated_location);
